3679c9b429dc0bbca74f6918f42fba40bb884cc3
Chenlin Fan <fancl@chromium.org>
2022-01-18 00:04:28 +1100

    Change copyright name

--- fslock/flock_linux.go
+++ fslock/flock_linux.go
@@ -1,4 +1,4 @@
-// Copyright 2022 by Dan Jacques. All rights reserved.
+// Copyright 2022 by Chenlin Fan. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
